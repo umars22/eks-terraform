f43dcb4bca64ee7145e5768a164ff3df6f306339
umars22 <70391013+umars22@users.noreply.github.com>
2023-07-24 19:15:58 -0400

    Create userdata.sh

---
+++ userdata.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#installs kubeclt
+
+sudo curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.27.1/2023-04-19/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
+
+
+#installs eksctl
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+sudo curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+sudo tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo mv /tmp/eksctl /usr/local/bin
